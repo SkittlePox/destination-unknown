28f5d6f5134ab434d45def1af4caa78efb8a8ba9
SkittlePox <bspiegel11@gmail.com>
2015-10-24 15:59:56 -0400

    Started on an event object

--- src/com/company/event.java
+++ src/com/company/event.java
@@ -2,17 +2,17 @@ package com.company;
 
 public class event {
 
-    int [] heldItems, roomItems;
-    boolean triggered;
+    int[] heldItems, roomItems, deadNcps;
+    boolean triggered, repeat;
 
-    public event(int[] itemsHeldNeeded, int[] itemsRoomNeeded) {
+    public event(int[] itemsHeldNeeded, int[] itemsRoomNeeded, int[] npcsDead, boolean repeated) {
         heldItems = itemsHeldNeeded;
         roomItems = itemsRoomNeeded;
+        deadNcps = npcsDead;
+        repeat = repeated;
     }
 
     public void test() {
-        if(!triggered){
 
-        }
     }
 }
